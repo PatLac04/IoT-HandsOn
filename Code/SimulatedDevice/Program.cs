ec98ced0c38653cb69fd73b8c9cdeabfd9370646
PatLac04 <patrice.lacroix@outlook.com>
2018-09-24 20:09:34 -0400

    Initial Revision

---
+++ Code/SimulatedDevice/Program.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        private static DeviceClient s_deviceClient;
+        private readonly static string s_myDeviceId = "Contoso-Test-Device";
+        private readonly static string s_iotHubUri = "{IoTHub Name}.azure-devices.net";
+        // This is the primary key for the device. This is in the portal. 
+        // Find your IoT hub in the portal > IoT devices > select your device > copy the key. 
+        private readonly static string s_deviceKey = "{ Device Key }";
+
+        private static int s_telemetryInterval = 1; // Seconds
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Routing Tutorial: Simulated device\n");
+            s_deviceClient = DeviceClient.Create(s_iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(s_myDeviceId, s_deviceKey), TransportType.Mqtt);
+
+            // Create a handler for the direct method call
+            s_deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
+
+            SendDeviceToCloudMessagesAsync();
+
+            Console.WriteLine("Press the Enter key to stop.");
+            Console.ReadLine();
+        }
+
+        // Handle the direct method call
+        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest methodRequest, object userContext)
+        {
+            var data = Encoding.UTF8.GetString(methodRequest.Data);
+
+            // Check the payload is a single integer value
+            if (Int32.TryParse(data, out s_telemetryInterval))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Telemetry interval set to {0} seconds", data);
+                Console.ResetColor();
+
+                // Acknowlege the direct method call with a 200 success message
+                string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+            }
+            else
+            {
+                // Acknowlege the direct method call with a 400 error message
+                string result = "{\"result\":\"Invalid parameter\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 400));
+            }
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                string infoString;
+                string levelValue;
+
+                if (rand.NextDouble() > 0.5)
+                {
+                    if (rand.NextDouble() > 0.7)
+                    {
+                        levelValue = "critical";
+                        infoString = "This is a critical message.";
+                    }
+                    else
+                    {
+                        levelValue = "storage";
+                        infoString = "This is a storage message.";
+                    }
+                }
+                else
+                {
+                    levelValue = "normal";
+                    infoString = "This is a normal message.";
+                }
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = s_myDeviceId,
+                    temperature = currentTemperature,
+                    humidity = currentHumidity,
+                    pointInfo = infoString
+                };
+                var telemetryDataString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                //set the body of the message to the serialized value of the telemetry data
+                var message = new Message(Encoding.ASCII.GetBytes(telemetryDataString));
+                message.Properties.Add("level", levelValue);
+
+                await s_deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, telemetryDataString);
+
+                await Task.Delay(s_telemetryInterval * 1000);
+            }
+        }
+    }
+}
